488303d48dca397501c6413793ed52df8c40d9e0
ingan <isabelngan27@gmail.com>
2019-11-04 13:21:58 -0800

    adding more empty base files

---
+++ client/constants/Colors.js
@@ -0,0 +1,14 @@
+const tintColor = '#416B75';
+
+export default {
+  tintColor,
+  tabIconDefault: '#8CAFAF',
+  tabIconSelected: '#0B3606',
+  tabBar: '#FFF5E9',
+  errorBackground: 'red',
+  errorText: '#C88276',
+  warningBackground: '#F3DFC6',
+  warningText: '#B27044',
+  noticeBackground: tintColor,
+  noticeText: '#C88276',
+};
