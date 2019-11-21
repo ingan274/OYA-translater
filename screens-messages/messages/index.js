a3d9a70a13ba75aa1c718d6ebf8e802a734e7a52
ingan <isabelngan27@gmail.com>
2019-11-20 19:33:49 -0800

    adding socket stuff

--- screens-messages/messages/index.js
+++ screens-messages/messages/index.js
@@ -12,9 +12,9 @@ class ChatRoom extends Component {
     this.state = {
       isConnected: false,
       messages: [],
-      Socket: '1234',
-      Volunteer: true,
-      User: false,
+      socketNum: '1234',
+      volunteer: true,
+      user: false,
       userId: '1234'
     };
 
@@ -40,12 +40,12 @@ class ChatRoom extends Component {
       let socket = await AsyncStorage.getItem('Vsocket' || 'socket');
 
       if (volunteer) {
-        this.setState({ Volunteer: true })
+        this.setState({ volunteer: true })
         this.setState({ userId: socket })
         this.setMySocket(socket);
         this.determineUser();
       } else {
-        this.setState({ User: true })
+        this.setState({ user: true })
         this.setMySocket(socket);
         this.setState({ userId: Math.round(Math.random() * 10000) })
       }
@@ -64,7 +64,7 @@ class ChatRoom extends Component {
   setMySocket = async (socket) => {
     // GET SOCKET ID AND SET THE STATE in local storage
     try {
-      this.setState({ Socket: socket })
+      this.setState({ socketNum: socket })
       this.socketEvents()
     } catch (error) {
       // Error retrieving data
@@ -73,7 +73,7 @@ class ChatRoom extends Component {
   }
 
   socketEvents = () => {
-    let socket = io(`${chat.serverIP}/socket/talk/${this.socket}`, {
+    let socket = io(`${chat.serverIP}/socket/talk/${this.state.socketNum}`, {
       transports: ['websocket'],
     });
 
